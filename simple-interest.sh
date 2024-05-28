ce792cb65138ad02e857ac46d2f1fb38cdb11311
MAHESH ADDAGATLA <158994925+Mahesh-addagatla@users.noreply.github.com>
2024-05-28 21:47:38 +0530

    Create simple-interest.sh

---
+++ simple-interest.sh
@@ -0,0 +1,8 @@
+$ echo '#!/bin/sh' > my-script.sh
+$ echo 'echo Hello World' >> my-script.sh
+$ cat my-script.sh
+#!/bin/sh
+echo Hello World
+$ chmod 755 my-script.sh
+$ ./my-script.sh
+Hello World
